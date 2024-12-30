56ca5ab6fdadf805950420fd648eb239100e6677
Yulian Gyurov <yg.atanasoff@gmail.com>
2024-12-30 20:54:07 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Yulian Gyuroff on 30.12.24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
