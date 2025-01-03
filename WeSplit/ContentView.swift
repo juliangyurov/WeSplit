3245b27f4e2573179e50787e480fcf5ff629a8bc
Yulian Gyurov <yg.atanasoff@gmail.com>
2025-01-03 20:21:20 +0200

    Merge remote-tracking branch 'refs/remotes/origin/main'

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,33 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    let students = ["Harry","Hermione","Ron"]
+    @State private var selectedStudent = "Harry"
     @State private var name = ""
     var body: some View {
-        VStack {
-            Form{
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.red)
-                //.padding()
-                Text("Hello, world!")
-                //.padding()
-            }
-            Form{
-                ForEach(1..<4) {
-                    Text("Row \($0)")
+        NavigationStack{
+            VStack {
+                Form{
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundStyle(.red)
+                    //.padding()
+                    Text("Hello, world!")
+                    //.padding()
+                }
+                Form{
+                    ForEach(1..<4) {
+                        Text("Row \($0)")
+                    }
+                }
+                Form{
+                    TextField("Enter your name", text: $name)
+                    Text("Hello \(name)!")
+                }
+                
+                Form{
+                    Picker("Select a student",selection: $selectedStudent) {
+                        ForEach(students, id: \.self) {
+                            Text($0)
+                        }
+                    }
                 }
             }
-            Form{
-                TextField("Enter your name", text: $name)
-                Text("Hello \(name)!")
-                }
-            Form{
-                Text("Hello \(name)!")
-            }
-            }
+            .navigationTitle("Select a Student")
         }
-        //.padding()
+        
     }
+    //.padding()
+}
 
 
 #Preview {
